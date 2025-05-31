a9857276bb3e5243fc8255dadce3408d32133f69
Cristian <criscamcr7@gmail.com>
2025-05-31 12:54:53 -0500

    Final version, Blogger App

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,2 @@
-echo "BUILD START"
-python3.9 -m pip install --upgrade pip
 pip install -r requirements.txt
-
-python3.9 manage.py collectstatic --noinput --clear
-echo "BUILD END"
+python3 manage.py collectstatic --noinput
