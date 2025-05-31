dafd84c83e891271d803fb1a4fe8fc75cd417af7
Cristian <criscamcr7@gmail.com>
2025-05-31 13:16:38 -0500

    Prueba

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,7 @@
 echo "BUILD START"
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput --clear
+
+pip install -r requirements.txt
+
+python manage.py collectstatic --noinput
+
 echo "BUILD END"
